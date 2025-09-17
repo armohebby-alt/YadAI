866a9ff073e97ba9e724e90dbf7986f1d3c6c897
armohebby-alt <a.r.mohebby@gmail.com>
2025-09-17 13:50:06 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 document.querySelector("button").addEventListener("click", () => {
   const lesson = document.querySelector("input[type='text']").value;
   const time = document.querySelector("input[type='time']").value;
+
   if (lesson && time) {
-    alert(`درس "${lesson}" برای ساعت ${time} ثبت شد.`);
+    const list = document.getElementById("lessonList");
+    const item = document.createElement("li");
+    item.textContent = `درس "${lesson}" برای ${time} ثبت شد.`;
+    list.appendChild(item);
   } else {
     alert("لطفاً نام درس و زمان را وارد کنید.");
   }
